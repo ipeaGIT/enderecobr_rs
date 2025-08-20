db8882c291c60940782d2d6e77d62353508030cf
Gabriel Garcia de Almeida <gabriel.almeida@ipea.gov.br>
2025-08-20 16:18:05 -0300

    feat: testes iniciais de performance

--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,43 @@
 use enderecobr_rs::padronizar_logradouros;
+use std::{collections::HashSet, io, time::SystemTime};
+
+// Roubei da internet
+fn timeit<F: Fn() -> T, T>(f: F) -> T {
+    let start = SystemTime::now();
+    let result = f();
+    let end = SystemTime::now();
+    let duration = end.duration_since(start).unwrap();
+    println!("Função demorou {} milis", duration.as_millis());
+    result
+}
+
+fn load_data() -> Vec<String> {
+    let mut rdr = csv::Reader::from_reader(io::stdin());
+    let mut vetor = Vec::<String>::new();
+
+    for result in rdr.records() {
+        let record = result.unwrap();
+        let logr = record.get(1).unwrap();
+        vetor.push(logr.to_string());
+    }
+    vetor
+}
+
+fn process_data(vetor: &[String]) {
+    let _res: Vec<String> = vetor
+        .iter()
+        .collect::<HashSet<&String>>()
+        .into_iter()
+        .map(|x| padronizar_logradouros(x.to_string()))
+        .collect();
+}
 
 fn main() {
-    let vetor = [
-        " av 122.123",
-        "rod bbbb, 123",
-        "mal.. 123.123123 JAn",
-        "11111 aaaa",
-    ];
-    for a in vetor {
-        println!("{}", padronizar_logradouros(a.to_string()));
-    }
+    let vetor = timeit(load_data);
+    timeit(|| process_data(&vetor));
+    println!(
+        "Finalizado para {} registros ({} unicos)",
+        vetor.len(),
+        vetor.iter().collect::<HashSet<&String>>().len(),
+    );
 }
