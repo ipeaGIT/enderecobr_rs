856d1c69feed381e0449fc35a77d5520088dc35e
Gabriel Garcia de Almeida <gabriel.almeida@ipea.gov.br>
2025-10-23 16:00:55 -0300

    feat: uso do clap para argumentos de linha de comando

--- src/bin/teste-polars-comparativo.rs
+++ src/bin/teste-polars-comparativo.rs
@@ -1,25 +1,43 @@
 use std::num::NonZero;
 
+use clap::Parser;
 use enderecobr_rs::obter_padronizador_por_tipo;
 use polars::prelude::{
     Column, CsvWriterOptions, DataType, Field, IntoColumn, LazyFrame, PlPath, ScanArgsParquet,
     SinkOptions, SinkTarget, StringChunked, col,
 };
 
+#[derive(Debug, Parser)]
+#[clap(author, version, about("Comparador"), long_about = None)]
+struct Args {
+    #[clap(help("Arquivo de entrada"))]
+    arquivo_entrada: String,
+    #[clap(short, long, default_value = "./diff.csv", help("Arquivo saída"))]
+    arquivo_saida: String,
+    #[clap(
+        short,
+        long,
+        default_value = "logradouro",
+        help("Tipo de Padronizador")
+    )]
+    tipo_padronizador: String,
+    #[clap(long, default_value = "logradouro", help("Campo com valor bruto"))]
+    campo_bruto: String,
+    #[clap(
+        long,
+        default_value = "logradouro_padr",
+        help("Campo com valor a ser comparado")
+    )]
+    campo_baseline: String,
+}
+
 fn main() {
-    let mut args = std::env::args();
-    let arquivo = args.next_back().unwrap();
-    let path = PlPath::new(&arquivo);
+    let args = Args::parse();
 
-    let campo_bruto = "logradouro";
-    let campo_baseline = "logradouro_padr";
-
-    let tipo_padronizador = "logradouro";
-
-    let padronizador = obter_padronizador_por_tipo(tipo_padronizador).unwrap();
+    let padronizador = obter_padronizador_por_tipo(&args.tipo_padronizador).unwrap();
 
     let res = LazyFrame::scan_parquet(
-        path,
+        PlPath::from_string(args.arquivo_entrada),
         ScanArgsParquet {
             low_memory: true,
             parallel: polars::prelude::ParallelStrategy::RowGroups,
@@ -30,9 +48,9 @@ fn main() {
     )
     .unwrap()
     .with_new_streaming(true)
-    .select([col(campo_bruto), col(campo_baseline)])
+    .select([col(&args.campo_bruto), col(&args.campo_baseline)])
     .with_column(
-        col(campo_bruto)
+        col(&args.campo_bruto)
             .map(
                 move |campo: Column| {
                     let iterador = campo.str().unwrap().iter().map(|opt| opt.map(padronizador));
@@ -43,10 +61,10 @@ fn main() {
             )
             .alias("campo_processado"),
     )
-    .filter(col("campo_processado").eq(col(campo_baseline)).not())
+    .filter(col("campo_processado").eq(col(&args.campo_baseline)).not())
     .unique(None, polars::frame::UniqueKeepStrategy::First)
     .sink_csv(
-        SinkTarget::Path(PlPath::new("/dev/stdout")),
+        SinkTarget::Path(PlPath::new(&args.arquivo_saida)),
         CsvWriterOptions {
             batch_size: NonZero::new(100).unwrap(),
             ..Default::default()
