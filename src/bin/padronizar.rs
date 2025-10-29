512fb45245d296ebd382ab76244cea40841c61bc
Gabriel Garcia de Almeida <gabriel.almeida@ipea.gov.br>
2025-10-29 11:41:27 -0300

    feat: Makefile para simplificar execução de tarefas rotineiras

--- src/bin/padronizar.rs
+++ src/bin/padronizar.rs
@@ -2,14 +2,16 @@ use clap::Parser;
 use enderecobr_rs::obter_padronizador_por_tipo;
 use std::io::{self, BufRead};
 
+/// Utilitário iterativo para realizar pequenos testes de linha de comando com os padronizadores e
+/// demais utilitários desta biblioteca.
 #[derive(Parser)]
-#[clap(author, version, about("Padronizador de endereços simplificado"), long_about = None)]
+#[clap(author, version)]
 struct Args {
-    /// Tipo de padronizador. Aceita: logradouro, logr, numero, num, bairro, complemento, comp, estado, estado_nome, estado_codigo, municipio, mun, cep, cep_leniente, completo, separar
+    /// Tipo de padronizador. Aceita: logradouro, logr, tipo_logradouro, tipo_logr, numero, num, bairro, complemento, comp, estado, estado_nome, estado_codigo, municipio, mun, cep, cep_leniente, completo, separar, separar_padronizar
     #[arg(value_enum)]
     tipo: String,
 
-    /// Valor a ser padronizado (opcional, lê de stdin se ausente)
+    /// Valor a ser padronizado (opcional, lê de stdin iterativamente se ausente)
     valor: Option<String>,
 }
 
