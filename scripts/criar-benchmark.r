20f52b9dbfc83643769c8582c54555a46999bc03
Gabriel Garcia de Almeida <gabriel.almeida@ipea.gov.br>
2025-10-29 09:32:59 -0300

    fix: sql geradora do benchmark com parametro errado

--- scripts/criar-benchmark.r
+++ scripts/criar-benchmark.r
@@ -23,7 +23,7 @@ FROM
 	read_csv('https://aplicacoes.mds.gov.br/sagi/servicos/misocial_mes_equipamento/?fl=codigo_ibge%2Canomes_s%20cneas_entidade_nome_municipio_s%20cneas_entidade_sigla_uf_s%20cneas_cod_entidade_s%20cneas_entidade_numero_cnpj_s%20cneas_entidade_razao_social_s%20cneas_entidade_nome_fantasia_s%20cneas_entidade_endereco_logradouro_s%20cneas_entidade_endereco_numero_s%20cneas_entidade_endereco_complemento_s%20cneas_entidade_endereco_bairro_s%20cneas_entidade_endereco_cep_s%20cneas_entidade_situacao_cadastro_s%20cneas_entidade_dt_extracao_s&fq=cneas_cod_entidade_s%3A*&q=*%3A*&rows=100000&sort=anomes_s%20desc%2C%20codigo_ibge%20asc&wt=csv&fq=anomes_s:2025*')
 UNION
 SELECT
-	concat_ws(no_tip_logradouro_fam, no_tit_logradouro_fam, no_logradouro_fam) AS logradouro,
+	concat_ws(' ', no_tip_logradouro_fam, no_tit_logradouro_fam, no_logradouro_fam) AS logradouro,
 	nu_logradouro_fam AS numero,
 	ds_complemento_fam AS complemento,
 	no_localidade_fam AS bairro,
@@ -35,7 +35,7 @@ FROM
 	read_parquet('//storage6/bases/DADOS/RESTRITO/CADASTRO_UNICO/parquet/cad_familia_072024.parquet')
 UNION
 SELECT
-	concat_ws(tipo_logradouro, logradouro) as logradouro,
+	concat_ws(' ', tipo_logradouro, logradouro) as logradouro,
 	numero_estab as numero,
 	complemento,
 	bairro,
